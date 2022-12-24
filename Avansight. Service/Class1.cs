de3754b166c88e9481fbf1d0441e5ae2f0da5f7e
ADHIKARI1 <samithaadikari@gmail.com>
2022-12-24 13:16:34 +0530

    init

---
+++ Avansight. Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Avansight._Service
+{
+    public class Class1
+    {
+    }
+}
---
+++ Avansight.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Avansight.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
