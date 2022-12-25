331977f0873a593b9b69ef2519bbbb4ad7adfbc5
ADHIKARI1 <samithaadikari@gmail.com>
2022-12-25 16:11:34 +0530

    changes

---
+++ Avansight.Domain/Migrations/20221224195841_storedproceudres.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Avansight.Domain.Migrations
+{
+    public partial class storedproceudres : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE PROCEDURE [dbo].[StudyImport]	
+									@Study AS StudiesTableType READONLY,
+									@TreatmentGroups AS TreatmentGroupsTableType READONLY
+								AS
+								BEGIN	
+									INSERT INTO Studies(StudyIdentifier, StudyName, ProjectNumber, Type)
+									SELECT StudyIdentifier, StudyName, ProjectNumber, Type FROM @Study;
+									INSERT INTO TreatmentGroups(TreatmentName, TreatmentColor)
+									SELECT TreatmentName, TreatmentColor FROM @TreatmentGroups;
+								END
+								GO
+								CREATE PROCEDURE [dbo].[PatientSet]
+									@Patients AS PatientsTableType READONLY
+								AS
+								BEGIN
+									INSERT INTO Patients(Age,Gender, StudyId, TreatmentCode)   
+									SELECT Age,Gender, StudyId, TreatmentCode from @Patients;	
+								END
+								GO
+								CREATE TRIGGER  Trigger_Patients_ForInsertmagic 
+								ON Patients  
+								FOR INSERT  
+								AS  
+								begin  
+								SELECT * FROM INSERTED
+								end 
+								GO
+								CREATE PROCEDURE [dbo].[PatientGet]
+								(
+									@StudyId BIGINT,
+									@TreatmentCode NVARCHAR(MAX) = Null,
+									@Age NVARCHAR(MAX) = Null,
+									@Gender BIGINT = Null    
+								)AS
+								BEGIN
+									SELECT *
+									FROM Patients
+									WHERE StudyId = @StudyId OR TreatmentCode LIKE '%'+@TreatmentCode+'%' OR Age LIKE '%'+@Age+'%' OR Gender = @Gender;
+								END
+								GO");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Avansight.Web/Controllers/StudyController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Avansight.Web.Controllers
+{
+    public class StudyController : Controller
+    {
+        // GET: StudyController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: StudyController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: StudyController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudyController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudyController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: StudyController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudyController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: StudyController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
